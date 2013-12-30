c8bc9fa772ddfb2b6d6424ad6305b99d308ef673
Sean Parr <seancparr@gmail.com>
2013-12-30 05:09:22 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Peeramid::Application.config.secret_key_base = '554b945bfc31151e009c9188bd93bf666077cc321f400d13fd0c024b8d0385bedb6f4a3f9bc00ee5a3d0b2c37bdb554f22656e7b0a3e706f0d00e85a0df573da'
