7726a84fa85ee49c2302819f24ef0f7b5783622e
Sean Parr <seancparr@gmail.com>
2014-01-10 13:11:43 -0500

    Create Specs

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,4 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   has_many :progressions
-  has_many :lessons, :sections, :courses, :through => :progressions
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,6 +2,4 @@ require 'spec_helper'
 
 describe User do
 
-	it 'should '
-
 end
