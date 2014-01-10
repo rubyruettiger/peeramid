4841b3f0576bb0ee24beeec581e72640b9b45658
Sean Parr <seancparr@gmail.com>
2014-01-10 16:32:10 -0500

    Tutorship Spec Tests

--- app/models/tutorship.rb
+++ app/models/tutorship.rb
@@ -1,4 +1,8 @@
 class Tutorship < ActiveRecord::Base
+
 	belongs_to :student, class_name: "User"
 	belongs_to :tutor, class_name: "User"
+
+	validates :student_id, presence: true
+	validates :tutor_id, presence: true
 end
--- spec/models/tutorship_spec.rb
+++ spec/models/tutorship_spec.rb
@@ -9,4 +9,20 @@ describe Tutorship do
   subject { tutorship }
 
   it { should be_valid }
+
+  describe 'tutorship can connect to users' do
+  	@user_a = User.create!([:email => "shithead@example.com", :password => "blahblahblah"])
+  	@user_b = User.create!([:email => "loser@example.com", :password => "blahewlda"])
+  	@user_a.tutorships.build(tutor_id: @user_b.id)
+
+  	@user_b.students.should include(@user_a)
+  end
+
+	describe 'tutorship methods' do
+		it { should respond_to(:student) }
+		it { should respond_to(:tutor) }
+		its(:student) { should eq user_a }
+		its(:tutor) { should eq user_b }
+	end
+
 end
