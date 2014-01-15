ac741ec49a19e5e6eb8c6fcbd5790c7dc9e3d4d2
Conor White-Sullivan <cwhitesullivan@gmail.com>
2014-01-14 18:36:40 -0800

    pending tests for progressions, their completeness, and their viewability

--- spec/controllers/lessons_controller_spec.rb
+++ spec/controllers/lessons_controller_spec.rb
@@ -9,8 +9,36 @@ describe LessonsController do
 		current_user.progressions.first.progressable.should eql(lesson)
 	end
 
+	pending "figure out how to actually test the custom routes and what is being passed there"
+
+	context "a progression exists for the current_user and the lesson" do
+
+		pending "create a mark_completed route that lets user change their mark to completed -- and alerts them that they will have a test "
+
+		pending "marked completed alerts mentor via email if mentor exists"
+
+		context "and the progression is marked completed" do
+
+			pending "and the progression is marked completed, fill in a bar in a progress bar for that lesson"
+
+		end
+
+	end
 
 
-	pending "if a progression exists for a lessson and the current user, and the progression is marked completed, fill in a bar in a progress bar for that lesson"
+
+
+
+
+	context "for way later" do
+
+		pending "potentially dry up the start route for other things -- for right now ok to just start lessons"
+
+
+		pending "create a mark_approved action that teachers can do to students that marks progressions approved"
+
+	end
+
+
 
 end
